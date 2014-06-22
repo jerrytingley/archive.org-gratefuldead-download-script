cb5c7ab13380049a07fc2bf9ac107b6de20869d2
jerrytingley <barry.mingley@gmail.com>
2014-06-21 21:38:32 -0400

    arghdos help updates
    Tuned up some parts, still need to work on the file resume ability so it either re-downloads the last downloaded files (hidden file id?) or find a simple way to download linearly to make downloaded_files[0:-1] exclude the last downloaded file due to position in list

--- archivemusicdownload.py
+++ archivemusicdownload.py
@@ -1,10 +1,12 @@
 import os
+import re
 import sys
 import json
 from urllib import urlretrieve
 from urllib2 import urlopen
 
-from pprint import pprint as p
+sanitize    = lambda filename: filename.replace('/', '_')
+nt_sanitize = lambda filename: re.sub('[<>:\"/\|\?*]', ' ', filename)
 
 def get_metadata(url):
 	# Parse and reformat metadata
@@ -24,18 +26,39 @@ def get_metadata(url):
 				new_file['local_filename'] = "%s. %s" % (file_data['track'], file_data['title'])
 			except KeyError:
 				new_file['local_filename'] = file_data['title']
+			new_file['local_filename'] = sanitize(new_file['local_filename'])
 			new_metadata['files'].append(new_file)
 	return new_metadata
 
 def download(url):
+	is_windows = os.name == 'nt'
+	print "[..] Downloading "+url
 	print "[..] Retrieving metadata..."
 	metadata = get_metadata(url)
-	print "[..] Creating directory..."
-	os.mkdir(metadata['local_download_dir'])
+	local_download_dir = metadata['local_download_dir']
+	if is_windows: local_download_dir = nt_sanitize(local_download_dir)
+	print "[..] Checking for existing download..."
+	resume = False
+	if (os.path.isdir(local_download_dir)):
+		# Ghetto file resume, might break with certain shows but yolo
+		# TODO:
+		# If the dl gets interupted, it's rare for the last file to be 100% dl'ed,
+		# so I'm having the script re-download the last downloaded file
+		print "[..] Found existing directory..."
+		resume = True
+		downloaded_files = os.listdir(os.path.dirname(os.path.realpath(__file__))+'/'+local_download_dir)
+		downloaded_files.sort()
+		downloaded_files = downloaded_files
+		print downloaded_files
+	else:
+		print "[..] Creating directory..."
+		os.mkdir(local_download_dir)
 	print "[..] Downloading files..."
 	file_count = len(metadata['files'])
 	for i, file_data in enumerate(metadata['files']):
-		local_filename = "%s/%s" % (metadata['local_download_dir'], file_data['local_filename'])
+		local_filename = "%s/%s" % (local_download_dir, file_data['local_filename'])
+		if resume and file_data['local_filename'] in downloaded_files: continue
+		if is_windows: local_filename = nt_sanitize(local_filename)
 		print "[..] Downloading %s (%i/%i)..." % (file_data['local_filename'], i+1, file_count)
 		urlretrieve(metadata['download_base']+file_data['download_filename'], local_filename)
 
@@ -43,7 +66,9 @@ def main():
 	if len(sys.argv) < 2:
 		print "usage: python archivemusicdownload.py <archive.org ID>"
 	else:
-		url = sys.argv[1]
-		print "[..] Downloading "+url
-		download(url)
+		try:
+			url = sys.argv[1]
+			download(url)
+		except KeyboardInterrupt:
+			print "[!!] CTRL-C, Quitting..."
 main()
