32e4daf244e6ca057eab077ed4efd6bfbdf779de
jerrytingley <barry.mingley@gmail.com>
2014-06-23 18:33:12 -0400

    Final large release
    I hope this is the last big update, with any later updates being small updates.

--- archivemusicdownload.py
+++ archivemusicdownload.py
@@ -1,66 +1,77 @@
 import os
-import re
 import sys
 import json
 from urllib import urlretrieve
 from urllib2 import urlopen
 
-sanitize    = lambda filename: filename.replace('/', '_')
-nt_sanitize = lambda filename: re.sub('[<>:\"/\|\?*]', ' ', filename)
+# Hopefully this is the final version, with only minor tweaks from now on
+# I originally wrote this with compact-ness in mind but I rewrote it to
+# hopefully make it more readable.
+
+def sanitize(filename):
+	return filename.replace('->', '-]').replace('/', ' - ')
+
+def get_m3u(url):
+	url = 'https://archive.org/download/{0}/{0}_vbr.m3u'.format(url)
+	data = urlopen(url).read()
+	return data.split('\n')[0:-1]
 
 def get_metadata(url):
-	# Parse and reformat metadata
 	metadata = json.loads(urlopen('https://archive.org/metadata/'+url).read())
+	server = metadata['server']
+	dir_ = metadata['dir']
+	creator = metadata['metadata']['creator']
+	date = metadata['metadata']['date']
+	venue = metadata['metadata']['venue']
 	new_metadata = {
-		'download_base' : 'https://'+metadata['server']+metadata['dir']+'/', # py2 % formatting is werid with slashes
-		'local_download_dir' : "%s - %s - %s" % (metadata['metadata']['creator'], metadata['metadata']['date'], metadata['metadata']['venue']),
-		'files' : [],
+		'download_base' : 'https://{0}{1}/'.format(server, dir_),
+		'local_download_dir' : '{0} - {1} - {2}'.format(creator, date, venue),
 	}
 	for file_data in metadata['files']:
 		if file_data['format'] == 'VBR MP3':
-			new_file = {
-				'title' : file_data['title'],
-				'download_filename' : file_data['name'],
+			filename = file_data['name']
+			title = file_data['title']
+			track = file_data['track']
+			new_metadata[filename] = {
+				'title'             : title,
+				'download_filename' : filename
 			}
 			try:
-				new_file['local_filename'] = "%s. %s" % (file_data['track'], file_data['title'])
+				new_metadata[filename]['local_filename'] = "{0}. {1}".format(track, title)
 			except KeyError:
-				new_file['local_filename'] = file_data['title']
-			new_file['local_filename'] = sanitize(new_file['local_filename'])
-			new_metadata['files'].append(new_file)
+				new_metadata[filename]['local_filename'] = title
+			new_metadata[filename]['local_filename'] = sanitize(new_metadata[filename]['local_filename'])
 	return new_metadata
 
 def download(url):
-	is_windows = os.name == 'nt'
 	print "[..] Downloading "+url
 	print "[..] Retrieving metadata..."
 	metadata = get_metadata(url)
 	local_download_dir = metadata['local_download_dir']
-	if is_windows: local_download_dir = nt_sanitize(local_download_dir)
+	links = get_m3u(url)
 	print "[..] Checking for existing download..."
 	resume = False
+	downloaded_files = []
 	if (os.path.isdir(local_download_dir)):
-		# Ghetto file resume, might break with certain shows but yolo
-		# TODO:
-		# If the dl gets interupted, it's rare for the last file to be 100% dl'ed,
-		# so I'm having the script re-download the last downloaded file
 		print "[..] Found existing directory..."
 		resume = True
 		downloaded_files = os.listdir(os.path.dirname(os.path.realpath(__file__))+'/'+local_download_dir)
 		downloaded_files.sort()
-		downloaded_files = downloaded_files
-		print downloaded_files
+		downloaded_files = downloaded_files[0:-1]
 	else:
 		print "[..] Creating directory..."
 		os.mkdir(local_download_dir)
 	print "[..] Downloading files..."
-	file_count = len(metadata['files'])
-	for i, file_data in enumerate(metadata['files']):
-		local_filename = "%s/%s" % (local_download_dir, file_data['local_filename'])
-		if resume and file_data['local_filename'] in downloaded_files: continue
-		if is_windows: local_filename = nt_sanitize(local_filename)
-		print "[..] Downloading %s (%i/%i)..." % (file_data['local_filename'], i+1, file_count)
-		urlretrieve(metadata['download_base']+file_data['download_filename'], local_filename)
+	file_count = len(links)
+	for i, link in enumerate(links, 1):
+		link = link.split('/')[-1]
+		link_metadata = metadata[link]
+		archive_download_url = metadata['download_base']+link
+		local_filename = link_metadata['local_filename']
+		if resume and local_filename in downloaded_files: continue
+		local_download_name = '{0}/{0}'.format(local_download_dir, local_filename)
+		print "[..] Downloading {0} ({1}/{2})...".format(local_filename, i, file_count)
+		urlretrieve(archive_download_url, local_download_name)
 
 def main():
 	if len(sys.argv) < 2:
